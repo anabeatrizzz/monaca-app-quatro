790819cf73c631be8489ecb35f27c51910392039
anabeatrizzz <anaaugusto2012@bol.com.br>
2020-05-20 05:20:12 +0900

    Vídeo sobre plugin de código de barras terminado

--- www/components/plugins-2.js
+++ www/components/plugins-2.js
@@ -15,4 +15,31 @@ $(function(){
         alert(`Deu ruim pode causa disso: ${msg}`);
     }
   })
+
+  $("#cdb").click(function(){
+    cordova.plugins.barcodeScanner.scan(
+      function (resultado) {
+          alert("Temos um código de barras\n" +
+                "Resultado: " + resultado.text + "\n" +
+                "Formato: " + resultado.format + "\n" +
+                "Cancelado: " + resultado.cancelled);
+      },
+      function (erro) {
+          alert(`Escaneamento falhou por causa disso: ${erro}`);
+      },
+      {
+          preferFrontCamera: false,
+          showFlipCameraButton: true,
+          showTorchButton: true,
+          torchOn: false, // Lanterna ligada por padrão
+          saveHistory: true, // Android, salvar o historico de escaneamento (o padrão é false)
+          prompt: "Coloque um código de barras dentro da área de escaneamento",
+          resultDisplayDuration: 500, // Android, mostre o texto escaneado or x milisegundos, o padrão é 1500
+          formats: "QR_CODE, CODE_39",
+          orientation: "landscape",
+          disableAnimations: true,
+          disableSuccessBeep: false
+      }
+   );
+  })
 })
